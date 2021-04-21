aa957f8391f8e6331a4d33317263feaa569bf5dd
hafsyah <hafsyahhasibuan96@gmail.com>
2021-04-22 00:40:29 +0700

    commit js

---
+++ loops-function.js
@@ -0,0 +1,15 @@
+const printOdds = (arr)
+    =>
+{
+
+
+    arr
+        .forEach(
+            el
+                => {
+
+                if (el % 2 != 0) console.log(el);
+
+            });
+
+}
