9b262b29c9669259c644092b4db6e472d6db524e
hafsyah <hafsyahhasibuan96@gmail.com>
2021-04-22 01:03:03 +0700

    commit js

---
+++ quiz8-2.js
@@ -0,0 +1,18 @@
+let myMap = new Map();
+
+myMap.set('zero',
+    o
+);
+
+myMap.
+set
+('one', 1);
+
+for
+    (let [key, value] of myMap) {
+
+    console.log(`${
+        key
+    } = ${value}`);
+
+}
