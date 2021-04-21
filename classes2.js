a0aac6ab248d952876fb406f44982fcbd43223bb
hafsyah <hafsyahhasibuan96@gmail.com>
2021-04-22 00:53:52 +0700

    commit js

---
+++ classes2.js
@@ -0,0 +1,28 @@
+class
+Human {
+
+    constructor(name) {
+
+        this.name = name;
+
+    }
+
+}
+
+
+
+class Student
+    extends
+        Human {
+
+    constructor(name, age) {
+
+
+        super
+        (name);
+
+        this.age = age;
+
+    }
+
+}
