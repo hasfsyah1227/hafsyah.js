8673156c632f4d590c2ee384c7b0cfbcfe92c277
hafsyah <hafsyahhasibuan96@gmail.com>
2021-04-22 00:52:28 +0700

    commit js

---
+++ classes.js
@@ -0,0 +1,25 @@
+class Dog {
+
+    constructor(name) {
+
+
+        this
+            .name = name;
+
+    }
+
+    bark() {
+
+        console.log(this.
+                name
+            + ' barks.');
+
+    }
+
+}
+
+let d = new Dog('Rex');
+
+d.
+bark
+();
